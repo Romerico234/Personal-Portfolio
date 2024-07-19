f1fdb9e61280711ed77fa7995adaa36ea5cbe293
Romerico <mrh.jrd.234@gmail.com>
2024-07-18 23:31:04 -0400

    Edited navbar to use React Bootstrap navbar

--- personal-portfolio/src/components/navbar/Navbar.tsx
+++ personal-portfolio/src/components/navbar/Navbar.tsx
@@ -1,83 +1,55 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Navbar.css";
 
-export default function Navbar() {
+export default function NavbarComponent() {
     return (
-        <nav className="navbar navbar-expand navbar-light bg-light fixed-top">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#home">
-                    Romerico David Jr
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div
-                    className="collapse navbar-collapse"
+        <Navbar bg="light" expand="lg" fixed="top">
+            <Container fluid>
+                <Navbar.Brand href="#home">Romerico David Jr</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarSupportedContent" />
+                <Navbar.Collapse
                     id="navbarSupportedContent"
+                    className="justify-content-end"
                 >
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link custom-nav-link"
-                                href="#about-me"
-                            >
-                                About Me
-                                <img
-                                    src="../../../images/navbar-images/person.png"
-                                    alt="Home Page"
-                                    className="navbar-img"
-                                />
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link custom-nav-link"
-                                href="#work-experiences"
-                            >
-                                Work Experiences
-                                <img
-                                    src="../../../images/navbar-images/suitcase.png"
-                                    alt="Work Experiences!"
-                                    className="navbar-img"
-                                />
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link custom-nav-link"
-                                href="#skills"
-                            >
-                                Skills
-                                <img
-                                    src="../../../images/navbar-images/skills.png"
-                                    alt="Skills!"
-                                    className="navbar-img"
-                                />
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link custom-nav-link"
-                                href="#projects"
-                            >
-                                Projects
-                                <img
-                                    src="../../../images/navbar-images/folder.png"
-                                    alt="Projects!"
-                                    className="navbar-img"
-                                />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                    <Nav>
+                        <Nav.Link href="#about-me" className="custom-nav-link">
+                            About Me
+                            <img
+                                src="../../../images/navbar-images/person.png"
+                                alt="Home Page"
+                                className="navbar-img"
+                            />
+                        </Nav.Link>
+                        <Nav.Link
+                            href="#work-experiences"
+                            className="custom-nav-link"
+                        >
+                            Work Experiences
+                            <img
+                                src="../../../images/navbar-images/suitcase.png"
+                                alt="Work Experiences!"
+                                className="navbar-img"
+                            />
+                        </Nav.Link>
+                        <Nav.Link href="#skills" className="custom-nav-link">
+                            Skills
+                            <img
+                                src="../../../images/navbar-images/skills.png"
+                                alt="Skills!"
+                                className="navbar-img"
+                            />
+                        </Nav.Link>
+                        <Nav.Link href="#projects" className="custom-nav-link">
+                            Projects
+                            <img
+                                src="../../../images/navbar-images/folder.png"
+                                alt="Projects!"
+                                className="navbar-img"
+                            />
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }

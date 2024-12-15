0fc2c30de3f8ae2c3e417322208547643672eefb
Romerico <mrh.jrd.234@gmail.com>
2024-12-15 16:23:40 -0500

    updated projects, resume, and skills

--- personal-portfolio/src/pages/projects/ProjectsItem.tsx
+++ personal-portfolio/src/pages/projects/ProjectsItem.tsx
@@ -1,39 +1,36 @@
 interface Project {
-    id: number;
-    name: string;
-    description: string;
-    link: string; 
-    image: string;
+	id: number;
+	name: string;
+	description: string;
+	link: string;
+	image: string;
 }
 
 interface ProjectItemProps {
-    project: Project;
+	project: Project;
 }
 
 export default function ProjectsItem({ project }: ProjectItemProps) {
-    return (
-        <div className="col-md-4 mb-4">
-            <div className="card h-100">
-                <div className="position-relative">
-                    <img
-                        src={project.image}
-                        className="card-img-top"
-                        alt={`${project.name} image`}
-                    />
-                    <a
-                        href={project.link}
-                        className="github-icon"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <img src="../../../images/projects-images/github-icon.png" alt="GitHub link" />
-                    </a>
-                </div>
-                <div className="card-body">
-                    <h5 className="card-title">{project.name}</h5>
-                    <p className="card-text">{project.description}</p>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="col-md-4 mb-4">
+			<a
+				href={project.link}
+				target="_blank"
+				rel="noopener noreferrer"
+				className="card h-100 text-decoration-none"
+			>
+				<div className="position-relative">
+					<img
+						src={project.image}
+						className="card-img-top"
+						alt={`${project.name} image`}
+					/>
+				</div>
+				<div className="card-body">
+					<h5 className="card-title">{project.name}</h5>
+					<p className="card-text">{project.description}</p>
+				</div>
+			</a>
+		</div>
+	);
 }
